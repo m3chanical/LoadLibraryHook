1309f92b673d3fbd0c7ce6f01c9caa10e98ba166
sinbeard <etc@example.com>
2021-03-18 22:56:39 -0400

    change polyhook to use vcpkg, strip out unneeded stuff

--- LoadLibraryHook.cpp
+++ LoadLibraryHook.cpp
@@ -1,11 +1,6 @@
-﻿// LoadLibraryHook.cpp : Defines the entry point for the application.
-//
-
-#include <Windows.h>
-#include <LoadLibraryHook.h>
+﻿#include <Windows.h>
 #include <polyhook2/ZydisDisassembler.hpp>
 #include <polyhook2/Detour/x64Detour.hpp>
-#include <polyhook2/PE/IatHook.hpp>
 
 #include <cstdarg>
 #include <conio.h>
@@ -14,22 +9,17 @@ uint64_t hookLoadLibrary = NULL; // original LoadLibrary
 // IDA: LoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags)
 HMODULE WINAPI hk_LoadLibrary(LPCWSTR lpLibFileName, HANDLE hfile, DWORD dwFlags)
 {
-	printf("Loading file: %ws\n", lpLibFileName);
+	printf("Loading file: %ws HANDLE: %p Flags: %lu\n", lpLibFileName, hfile, dwFlags);
 	return PLH::FnCast(hookLoadLibrary, &LoadLibraryExW)(lpLibFileName, hfile, dwFlags);
-}
-
-int getch_noblock()
-{
-	if (_kbhit())
-		return _getch();
-	return -1;
 }
 
 int main()
 {
+	auto error_log = std::make_shared<PLH::ErrorLog>();
+	PLH::Log::registerLogger(error_log);
 	PLH::ZydisDisassembler dis(PLH::Mode::x64);
 
-	HMODULE k32 = GetModuleHandle("kernelbase.dll");
+	HMODULE k32 = GetModuleHandleA("kernelbase.dll");
 	if (k32 == NULL)
 		return 0;
 	// all of the loadlibrary functions in kernel32/kernelbase end up calling LoadLibraryExW, which is the last step before ntdll.
@@ -37,18 +27,20 @@ int main()
 	if (load_lib_addr == NULL)
 		return 0;
 	printf("LoadLibraryExW address in kernelbase.dll: %p\n", load_lib_addr);
-	
-	std::unique_ptr<PLH::x64Detour> p = 
-		std::make_unique<PLH::x64Detour>(reinterpret_cast<uint64_t>(load_lib_addr), reinterpret_cast<uint64_t>(&hk_LoadLibrary), &hookLoadLibrary, dis);	
-	
-	p->hook();
-	//LoadLibraryA("F:\\vs_projects\\test_inject\\x64\\Debug\\test_inject.dll");
+
+	PLH::x64Detour loadlib_detour(reinterpret_cast<uint64_t>(load_lib_addr), reinterpret_cast<uint64_t>(&hk_LoadLibrary), &hookLoadLibrary, dis);
+
+	loadlib_detour.hook();
+
+	LoadLibraryExW(L"kernel32.dll", 0, 0);
 
 	while (true)
 	{
-		if (getch_noblock() != -1)
+		if (getchar())
 			break;
 	}
-	p->unHook();
-	return 0;	
+
+	loadlib_detour.unHook();
+
+	return 0;
 }
